a38c5d9ac737e12c50eaa349e9ca29718be7d18a
Dmitry Parshukov <dimer3799@yandex.ru>
2020-11-12 14:22:17 +0600

    create logout

--- users/views.py
+++ users/views.py
@@ -8,8 +8,8 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Аккаунт создан, {username}!')
-            return redirect('blog-home')
+            messages.success(request, ''f'Ваш аккаунт создан, {username} успешно создан! Теперь вы можете войти в систему')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render (request, 'users/register.html', {'form': form})
