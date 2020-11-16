8ffc2b3c29ae8dd58faa2465c346327a60e36fd0
Dmitry Parshukov <dimer3799@yandex.ru>
2020-11-16 15:52:29 +0600

    create update size picter users logo

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE)
@@ -7,3 +8,15 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Профиль'
+
+    def save(self, *args, **kwargs):
+        # Переопределение метода сохранения изображений
+        super().save( *args, **kwargs)
+
+        img = Image.open(self.image.path)
+
+        if img.width > 300 or img.height > 300:
+            # Если высота или ширина изображения больше 300, изменяем размер и пересохраняем изображение
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
