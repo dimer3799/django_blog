98ddff194d7897b6097a04496c0002f615ced862
Dmitry Parshukov <dimer3799@yandex.ru>
2020-12-04 22:26:46 +0600

    create postand redirect detail post

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
+    title = models.CharField(max_length=100, verbose_name='Заголовок')
+    content = models.TextField(verbose_name='Содержание')
     date_posted = models.DateField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+    # Получения абсолютного url для поста
+        return reverse('post-detail', kwargs = {'pk': self.pk})
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from django.http import HttpResponse
 from .models import Post
 
 def home(request):
     context = {
-        'posts': Post.objects.all()
+        'post': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
@@ -17,6 +17,15 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'О блоге'})
